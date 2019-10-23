e172ca9e623e70f946813e5f7a8b767b1e2a155c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-23 16:17:36 -0400

    Configure Babel not to compile classes; Remove tutorial example

--- nwb.config.js
+++ nwb.config.js
@@ -8,5 +8,15 @@ module.exports = {
         react: 'React'
       }
     }
+  },
+  babel: {
+    env: {
+      targets: {
+        node: 'current'
+        // WebComponents require native ES class syntax,
+        // so we need to tell Babel to *not* transpile classes.
+        // NOTE: Not sure if this is the best setting for cross browser compatibility.
+      }
+    }
   }
 }
