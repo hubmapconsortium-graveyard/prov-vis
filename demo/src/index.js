390d9e36ff670463885674134e438a6abc3e697b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-24 10:56:48 -0400

    Add CSS to React demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 
 import hubmapProvVis from '../../src'
+import '@hms-dbmi-bgm/react-workflow-viz/dist/react-workflow-viz.min.css';
 
 class Demo extends Component {
   render() {
