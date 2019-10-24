6cc06e06cae504d103c7ea4082cbd8cf9275ae9b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-24 16:52:53 -0400

    linting

--- src/ProvVis.js
+++ src/ProvVis.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 import Graph, { GraphParser } from '@hms-dbmi-bgm/react-workflow-viz';
 
-import Prov from './Prov'
+import Prov from './Prov';
 
 export default function ProvVis(props) {
-  const {prov} = props;
-  const steps = new Prov(prov).toCwl()
+  const { prov } = props;
+  const steps = new Prov(prov).toCwl();
   return (
     <GraphParser
       parsingOptions={{
         parseBasicIO: false,
         showIndirectFiles: true,
         showParameters: false,
-        showReferenceFiles: true
+        showReferenceFiles: true,
       }}
       parentItem={{ name: 'Is this used?' }}
       steps={steps}
     >
-      <Graph rowSpacingType={'compact'} minimumHeight={300} />
+      <Graph rowSpacingType="compact" minimumHeight={300} />
     </GraphParser>
   );
 }
