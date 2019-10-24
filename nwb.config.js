fa1add92ac9bcbab85b56f758eb3d55cae31ace3
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-24 15:49:17 -0400

    Try headless chrome

--- nwb.config.js
+++ nwb.config.js
@@ -10,8 +10,17 @@ module.exports = {
     }
   },
   karma: {
-    browsers: ['Chrome']
-    // Chrome gives us visual debugging, and support for language features that aren't in PhantomJS.
-    // for example: Object.fromEntries
+    // Chrome gives us visual debugging, and support for language features
+    // that aren't in PhantomJS, like Object.fromEntries.
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    // ... but there are limitations on Travis. https://docs.travis-ci.com/user/chrome
+    extra: {
+      customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+        }
+      }
+    }
   }
 }
