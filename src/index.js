86967be7a2082a05aee4dab1e342a23472b306b5
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-24 17:18:15 -0400

    Get the UMD demo working, too

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 
 import ProvVis from './ProvVis';
 
-function renderProvVis(id) {
+function renderProvVis(id, prov) {
   const element = document.getElementById(id);
-  ReactDOM.render(<ProvVis />, element);
+  ReactDOM.render(<ProvVis prov={prov} />, element);
 }
 
 export default {
