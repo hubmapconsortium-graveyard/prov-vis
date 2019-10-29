a8504759fdc889254ee499cdd3ed1c2adc21e2ff
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-29 14:57:13 -0400

    Move complex example to fixtures

--- tests/Prov.test.js
+++ tests/Prov.test.js
@@ -4,6 +4,7 @@ import Prov, { makeCwlInput, makeCwlOutput } from '../src/Prov';
 
 import * as fixtures from './fixtures';
 
+
 describe('Prov fixtures', () => {
   Object.entries(fixtures).forEach(([k, v]) => {
     it(`converts ${k} W3C JSON to 4DN CWL`, () => {
@@ -30,70 +31,7 @@ describe('Prov errors', () => {
 });
 
 describe('Prov methods', () => {
-  /*
-  E1 ═> A1 ═> E3 ═> A3 ═> E5
-  ║╚═══════╗  ╚══════╗
-  ╚═════╗  ╚════════╗║
-        V           VV
-  E2 ═> A2 ═> E4 ═> A4 ═> E6
-        ║
-        ╚═══> E7
-  */
-  /* eslint-disable object-curly-spacing */
-  /* eslint-disable object-curly-newline */
-  /* eslint-disable indent */
-  const prov = new Prov({
-    prefix: {
-      hubmap: 'https://hubmapconsortium.org',
-    },
-    entity: {
-      'hubmap:ent-1': {'prov:label': 'ent-1'},
-      'hubmap:ent-2': {'prov:label': 'ent-2'},
-      'hubmap:ent-3': {'prov:label': 'ent-3'},
-      'hubmap:ent-4': {'prov:label': 'ent-4'},
-      'hubmap:ent-5': {'prov:label': 'ent-5'},
-      'hubmap:ent-6': {'prov:label': 'ent-6'},
-      'hubmap:ent-7': {'prov:label': 'ent-7'},
-    },
-    activity: {
-      'hubmap:act-1': {'prov:label': 'act-1'},
-      'hubmap:act-2': {'prov:label': 'act-2'},
-      'hubmap:act-3': {'prov:label': 'act-3'},
-      'hubmap:act-4': {'prov:label': 'act-4'},
-    },
-    wasGeneratedBy: {
-      '_:1': {'prov:entity': 'hubmap:ent-3',
-              'prov:activity': 'hubmap:act-1'},
-      '_:2': {'prov:entity': 'hubmap:ent-4',
-              'prov:activity': 'hubmap:act-2'},
-      '_:3': {'prov:entity': 'hubmap:ent-5',
-              'prov:activity': 'hubmap:act-3'},
-      '_:4': {'prov:entity': 'hubmap:ent-6',
-              'prov:activity': 'hubmap:act-4'},
-      '_:5': {'prov:entity': 'hubmap:ent-7',
-              'prov:activity': 'hubmap:act-2'},
-    },
-    used: {
-      '_:5': {'prov:activity': 'hubmap:act-1',
-              'prov:entity': 'hubmap:ent-1'},
-
-      '_:6': {'prov:activity': 'hubmap:act-2',
-              'prov:entity': 'hubmap:ent-1'},
-      '_:7': {'prov:activity': 'hubmap:act-2',
-              'prov:entity': 'hubmap:ent-2'},
-
-      '_:8': {'prov:activity': 'hubmap:act-3',
-              'prov:entity': 'hubmap:ent-3'},
-
-      '_:9': {'prov:activity': 'hubmap:act-4',
-              'prov:entity': 'hubmap:ent-1'},
-      '_:10': {'prov:activity': 'hubmap:act-4',
-              'prov:entity': 'hubmap:ent-3'},
-      '_:11': {'prov:activity': 'hubmap:act-4',
-              'prov:entity': 'hubmap:ent-4'},
-      },
-    });
-    /* eslint-enable */
+  const prov = new Prov(fixtures.complex.prov);
 
   it('getParentEntities', () => {
     expect(prov.getParentEntities('hubmap:act-4')).toEqual([
