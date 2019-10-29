1c1184a27fd098f4288f6376ea6c26054a7b13e4
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-29 15:43:08 -0400

    Better warning in script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ set -o errexit
 die() { set +v; echo "$*" 1>&2 ; exit 1; }
 
 [ `git symbolic-ref --short HEAD` != 'master' ] || die 'Make a release branch!'
-git status && die 'Commit changes!'
+git status && die 'Commit changes and push!'
 
 ./test.sh || die 'Fix tests!'
 
