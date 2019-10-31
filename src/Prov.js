31150281e58f01c988d50ddb278b9cbb5720af9e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-31 16:32:23 -0400

    Start filling in Provenance extras

--- src/Prov.js
+++ src/Prov.js
@@ -125,7 +125,7 @@ export default class Prov {
       name: activityName,
       inputs,
       outputs,
-      prov: { extras: 'go here' },
+      prov: this.prov.activity[activityId],
     };
   }
 
